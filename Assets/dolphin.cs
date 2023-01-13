92d836b0bfb84f00c350fd28b312a85c2aa04b86
DavveDS <119926959+DavveDS@users.noreply.github.com>
2023-01-13 11:18:37 +0100

    TEXT YEEEEEEEEEEEEEEEEEEEEEEE

--- Assets/dolphin.cs
+++ Assets/dolphin.cs
@@ -2,19 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class dolphin : MonoBehaviour
 {
+
+
     [SerializeField]
     Transform bomba;
+
+ 
+
     // Start is called before the first frame update
     void Start()
     {
         transform.position = bomba.position;
+     
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0, 1, 0)* Time.deltaTime;
+
     }
 }
