27005d6b58670888d536ea3382f1b2e53e68e3f1
Adrian <113990519+AdrianDeveloper@users.noreply.github.com>
2023-01-19 14:46:26 +0100

    Final tweaks

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,14 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    public Button restartButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Button btn = restartButton.GetComponent<Button>();
-    }
+    
+
+    
      // Restart is called on click
-    void Restart()
+   public void Restart()
     {
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
